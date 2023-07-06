165af36171857016a6ecbf656d8e8797fcaaa524
ajp84 <prater.anthony.j@gmail.com>
2023-07-05 20:29:58 -0700

    fonts and css variables

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
+import { fileURLToPath, URL } from 'url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+// import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // resolve:{
-  //   alias:{
-  //     "@components":
-  //       path.resolve(_dirname, 'src','components')
-  //   }
-  // },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: {
     host: true,
   },
