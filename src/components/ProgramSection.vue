7619a605e3e03c4cd7252f6ff846e749f6a1a120
ajp1984 <prater.anthony.j@gmail.com>
2023-07-24 17:49:40 +0000

    program list

--- src/components/ProgramSection.vue
+++ src/components/ProgramSection.vue
@@ -1,5 +1,43 @@
 <script setup>
+  import { ref } from 'vue'
+  import ProgramCard from './cards/ProgramCard.vue';
 
+  const props = defineProps({
+    programs: {
+      type: Array,
+      required: true,
+    },
+  });
+
+  const artists = props.artists;
+
+
+  const programs = ref([
+    {
+      'title': 'children',
+      'ages': 'From 6 to 9 years old',
+      'days': 'Monday/Thursday/Friday',
+      'time': '4pm-6pm',
+      'exceptions': 'conditions may change',
+      'id': '1'
+    },
+    {
+      'title': 'amateurs',
+      'ages': 'From 6 to 9 years old',
+      'days': 'Monday/Thursday/Friday',
+      'time': '4pm-6pm',
+      'exceptions': 'conditions may change',
+      'id': '2'
+    },
+    {
+      'title': 'professional',
+      'ages': 'From 6 to 9 years old',
+      'days': 'Monday/Thursday/Friday',
+      'time': '4pm-6pm',
+      'exceptions': 'conditions may change',
+      'id': '3'
+    },
+    ])
 </script>
 
 <template>
@@ -7,7 +45,7 @@
         <h2>Our Programs</h2>
         <p>We have individual programs for children, amateurs, and professionals. Our programs are tailored to the age and skill level of each group.</p>
         <div class="card-container">
-
+            <ProgramCard :programs="programs" />
         </div>
     </section>
 </template>
---
+++ src/components/cards/ProgramCard.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    programs: {
+      type: Array,
+      required: true,
+    },
+  });
+
+// const props = defineProps(['programs']);
+
+</script>
+
+<template>
+    <article v-for="program in programs" :key='program.id'>
+        <h3>{{ program.title }}</h3>
+        <img src="" alt="">
+        <ul>
+            <li>{{ program.ages }}</li>
+            <li>{{ program.days }}</li>
+            <li>{{ program.time }}</li>
+            <li>{{ program.exceptions }}</li>
+        </ul>
+        <a href="">Read More</a>
+    </article>
+</template>
+
+<style scoped>
+    article {
+        background: #fff;
+        color: var(--green);
+    }
+</style>
