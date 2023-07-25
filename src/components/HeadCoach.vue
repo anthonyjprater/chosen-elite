aa484f7dede03b67db4fc53deef0f1d050ae8318
ajp1984 <prater.anthony.j@gmail.com>
2023-07-25 19:16:33 +0000

    headcoach section initial commit

---
+++ src/components/HeadCoach.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="headcoach-bio">
+        <article>
+            <h2>Head coach -Timothy Prater </h2>
+            <p class="headcoach-details">Tim P. is an exceptional basketball training coach with a relentless dedication to the game. His expertise and motivating approach inspire athletes to surpass their limits, fostering a generation of skilled players. With a focus on fundamentals and strategic prowess, Tim P. has become a guiding force for aspiring ballers seeking to elevate their game to new heights. #BasketballCoach #TrainingGuru</p>
+        </article>
+    </section>
+</template>
+
+<style scoped>
+section {
+    background: rgba(47, 47, 47, 1);
+}
+</style>
