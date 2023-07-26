b2708bbc70f1997adb45e08f68edc12201c57a7e
ajp1984 <prater.anthony.j@gmail.com>
2023-07-26 19:26:53 +0000

    carousel heading

--- src/components/MainCarousel.vue
+++ src/components/MainCarousel.vue
@@ -4,12 +4,38 @@
 
 <template>
     <section>
-        <h2 class="pitch">Chosen Elite Academy is the best choice for you</h2>
+        <h2>Chosen Elite Academy</h2> 
+        <span class="pitch">is the best choice for you</span>
+        <div class="carousel-container">
+            <i class="fa-solid fa-basketball"></i>
+        </div>
     </section>
 </template>
 
 <style scoped>
-    section {
-        width: 100vw;
-    }
+section {
+    width: 100vw;
+}
+
+h2,
+h2 > span {
+    font-size: 1.2rem;
+}
+
+span.pitch {
+    font-weight: bold;
+    line-height: 1;
+}
+
+.carousel-container {
+    
+}
+
+.fa-basketball {
+    color: var(--grey);
+    font-size: 1.5rem;
+    padding: 1rem;
+    background: var(--green);
+    border-radius: 5px;
+}
 </style>
