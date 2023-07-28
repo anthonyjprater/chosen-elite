88891aaa582189802e59169968c5a21007177396
ajp1984 <prater.anthony.j@gmail.com>
2023-07-28 15:20:15 +0000

    main page layout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import MainHeader from './components/headers/MainHeader.vue'
-// import MainFooter from './components/footers/MainFooter.vue'
-// import MainHero from './components/heros/MainHero.vue';
-// import ProgramSection from './components/ProgramSection.vue';
-// import HeadCoach from './components/HeadCoach.vue';
+import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from './components/headers/MainHeader.vue'
+import MainFooter from './components/footers/MainFooter.vue'
+import MainHero from './components/heros/MainHero.vue';
+import ProgramSection from './components/ProgramSection.vue';
+import HeadCoach from './components/HeadCoach.vue';
 import MainCarousel from './components/MainCarousel.vue';
+
 </script>
 
 <template>
-  <!-- <MainHeader /> -->
-  <!-- <MainHero /> -->
-  <!-- <ProgramSection /> -->
-  <!-- <HeadCoach /> -->
-  <MainCarousel />
+  <MainHeader />
+  <main>
+
+    <MainHero />
+    <ProgramSection />
+    <HeadCoach />
+    <MainCarousel />
+  </main>
   <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -23,9 +27,11 @@ import MainCarousel from './components/MainCarousel.vue';
     </a>
   </div> -->
   <!-- <HelloWorld msg="Vite + Vue" /> -->
-  <!-- <MainFooter /> -->
+  <MainFooter />
 </template>
 
 <style scoped>
-  
+  main {
+    grid-area: main;
+  }
 </style>
