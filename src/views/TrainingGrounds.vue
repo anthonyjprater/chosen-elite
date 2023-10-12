88991ea62e45994bad5c099e27d67a57e85c93b0
ajp1984 <prater.anthony.j@gmail.com>
2023-10-12 19:12:44 +0000

    training grounds alignment

--- src/views/TrainingGrounds.vue
+++ src/views/TrainingGrounds.vue
@@ -17,30 +17,39 @@ const locations = ref([
 </script>
 
 <template>
-<h1></h1>
-<section>
-    <img src="" alt="">
-    <h2></h2>
-    <article>
-        <h3></h3>
-        <p></p>
-        <h3></h3>
-        <address></address>
-    </article>
+  <section>
+  <h1>Training Grounds</h1>
+    <img src="" alt="" width="280" height="160">
+    <div>
+      <h2>{{ locations[0].heading }}</h2>
+      <article>
+          <h3>Description</h3>
+          <p>{{ locations[0].text1 }}</p>
+          <h3>Address</h3>
+          <address>{{ locations[0].text2 }}</address>
+      </article>
+    </div>
 </section>
 </template>
 
 <style scoped>
+
+h1 {
+  text-align: left;
+}
+
 section {
     background: rgba(47, 47, 47, 1);
     display: flex;
     flex-wrap: wrap;
     align-items: baseline;
     justify-content: center;
+    text-align: left;
     width: 100vw;
     padding: 3rem 1.5rem;
     gap: 1.5rem;
     background: linear-gradient(0deg, rgba(47,47,47,.7), rgba(0,0,0,1));
     text-align: left;
 }
+
 </style>
