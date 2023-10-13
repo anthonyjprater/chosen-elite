39735015260743e23338eaed8ed60760aff8a027
ajp1984 <prater.anthony.j@gmail.com>
2023-10-13 19:23:59 +0000

    training grounds styles

--- src/views/TrainingGrounds.vue
+++ src/views/TrainingGrounds.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { ref } from 'vue';
+import MainHeader from '../components/headers/MainHeader.vue';
 
 const locations = ref([
     { id: 1,
+      image: 'court1.png',
       heading: 'Olympic View' ,
       text1: "A full-size professional basketball hall is located in the Innovation Center on the 4th floor of the PRO-Trainer Fitness Club. The gym is equipped with everything you need to train children and adults. cozy locker rooms, private lockers for changing and much more are available for Chosen Elite Basketball students.",
       text2: "2602 Mukilteo Speedway, Mukilteo, WA 98275" ,
     },
     { id: 2,
+      image: 'court2.png',
       heading: 'Olympic View' ,
       text1: "A full-size professional basketball hall is located in the Innovation Center on the 4th floor of the PRO-Trainer Fitness Club. The gym is equipped with everything you need to train children and adults. cozy locker rooms, private lockers for changing and much more are available for Chosen Elite Basketball students.",
       text2: "2602 Mukilteo Speedway, Mukilteo, WA 98275" ,
@@ -17,25 +20,28 @@ const locations = ref([
 </script>
 
 <template>
+  <MainHeader></MainHeader>
   <section>
-  <h1>Training Grounds</h1>
-    <img src="" alt="" width="280" height="160">
-    <div>
-      <h2>{{ locations[0].heading }}</h2>
-      <article>
-          <h3>Description</h3>
-          <p>{{ locations[0].text1 }}</p>
-          <h3>Address</h3>
-          <address>{{ locations[0].text2 }}</address>
-      </article>
-    </div>
-</section>
+    <h1>Training Grounds</h1>
+    <article v-for="location in locations" key='location.id'>
+      <img :src="location.image" alt="" width="280" height="160">
+      <div>
+        <h2>{{ location.heading }}</h2>
+            <h3>Description</h3>
+            <p>{{ location.text1 }}</p>
+            <h3>Address</h3>
+            <address>{{ location.text2 }}</address>  
+      </div>
+    </article>
+  </section>
 </template>
 
 <style scoped>
 
 h1 {
+  font-size: 1.5rem;
   text-align: left;
+  text-wrap: balance;
 }
 
 section {
@@ -43,13 +49,38 @@ section {
     display: flex;
     flex-wrap: wrap;
     align-items: baseline;
-    justify-content: center;
+    /* justify-content: center; */
     text-align: left;
     width: 100vw;
     padding: 3rem 1.5rem;
     gap: 1.5rem;
     background: linear-gradient(0deg, rgba(47,47,47,.7), rgba(0,0,0,1));
     text-align: left;
+}
+
+section article:not(:last-child) {
+  border-bottom: 2px solid var(--green);
+  padding: 1rem;
+}
+
+img,h2,h3,p {
+  margin-bottom: 1rem;
+}
+
+h2 {
+  font-size: 1.2rem;
+}
+
+h3 {
+  font-size: 1rem;
+  /* font-weight: normal; */
+  color: var(--green);
+}
+
+@media(min-width: 768px) {
+  h1 {
+    font-size: 2.5rem;
+  }
 }
 
 </style>
